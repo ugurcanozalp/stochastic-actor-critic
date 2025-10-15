e673a00b699aef3fc9523c866c4cab73a79dcd6d
uğurcan özalp <ugurcanozalp06@gmail.com>
2025-10-15 19:33:56 +0300

    updated figures and logs

--- get_results.py
+++ get_results.py
@@ -3,12 +3,19 @@ import os
 import sys
 
 from summarize import summarize
+import numpy as np
 
-summarize("logs/stac-fixed-pessimism", 
+
+summary_pessimism=summarize("logs/stac-fixed-pessimism", 
 	"results/stac-fixed-pessimism", (3, 3))
 
-#summarize("logs/stac-dropout", 
-#	"results/stac-dropout", (3, 3))
+summarize("logs/stac-dropout", 
+	"results/stac-dropout", (3, 3))
 
-summarize("logs/main-comparison", 
+summary_comparison=summarize("logs/main-comparison", 
 	"results/main-comparison", (3, 3))
+
+#print(summary_comparison.swaplevel(i=1,j=0).loc["avg_scores"].apply(lambda x: np.round(x, 1)) )
+#print(summary_comparison.swaplevel(i=1,j=0).loc["std_scores"].apply(lambda x: np.round(x, 1)) )
+#print(summary_comparison.swaplevel(i=1,j=0).loc["last_score_iqm"].apply(lambda x: np.round(x, 1)) )
+#print(summary_pessimism.swaplevel(i=1,j=0).loc["last_score_iqm"].apply(lambda x: np.round(x, 1)) )
